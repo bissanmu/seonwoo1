e350b5faff418790f6c9a7dc589b675d53758141
seonwoo <bissanmu@gmail.com>
2017-08-11 17:40:22 +0900

    코드로 화면전환 구현 완료

---
+++ seonwoo1/GrayViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GrayViewController.swift
+//  seonwoo1
+//
+//  Created by ktds 29  on 2017. 8. 11..
+//  Copyright © 2017년 ktds 29 . All rights reserved.
+//
+
+import UIKit
+
+class GrayViewController: UIViewController {
+
+    @IBAction func close(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
