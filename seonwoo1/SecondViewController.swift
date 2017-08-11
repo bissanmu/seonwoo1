97797be5f21443d287bd0b64254a44fa5aaa6945
seonwoo <bissanmu@gmail.com>
2017-08-11 16:46:45 +0900

    second

--- seonwoo1/SecondViewController.swift
+++ seonwoo1/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  seonwoo1
 //
 //  Created by ktds 29  on 2017. 8. 11..
@@ -8,21 +8,8 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class SecondViewController: UIViewController {
 
-    @IBOutlet weak var mySwitch: UISwitch!
-    
-    @IBAction func nextScene(_ sender: Any) {
-        
-        if mySwitch.isOn{
-            self.performSegue(withIdentifier: "second", sender: self)
-        }else{
-            self.performSegue(withIdentifier: "third", sender: self)
-        }
-        
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
